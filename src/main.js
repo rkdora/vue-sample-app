4847b3d2bbb259e359b63753aa2686b4bdb7a751
ryuto <rryuto5210@gmail.com>
2020-05-30 18:54:01 +0900

    Add firebase.config.js

--- src/main.js
+++ src/main.js
@@ -2,9 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import { firebaseConfig } from '../firebase.config.js'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 
 new Vue({
   router,
