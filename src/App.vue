446f42973cc126a09e0b171f936d7db382fc54b7
ryuto <rryuto5210@gmail.com>
2020-05-30 19:35:13 +0900

    Get login user info

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,7 @@
 </template>
 
 <script>
+import firebase from 'firebase'
 import { mapActions} from 'vuex'
 import SideNav from './components/SideNav'
 
@@ -30,12 +31,18 @@ export default {
   components: {
     SideNav
   },
-
+  created () {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setLoginUser(user)
+      }
+    })
+  },
   data: () => ({
     //
   }),
   methods: {
-    ...mapActions(['toggleSideMenu'])
+    ...mapActions(['toggleSideMenu', 'setLoginUser'])
   }
 };
 </script>
